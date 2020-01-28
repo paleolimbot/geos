1dd5c2aa1391bd50592a2a80d935ce4a75f31558
Dewey Dunnington <dewey@fishandwhistle.net>
2020-01-28 13:30:58 -0800

    add geo_wkt class

---
+++ R/geo-wkt.R
@@ -0,0 +1,40 @@
+
+#' Create and validate well-known text
+#'
+#' @param x A character vector containing well-known text
+#'
+#' @return A vctr of class geo_wkt
+#' @export
+#'
+#' @examples
+#' geo_wkt("POINT 30 10")
+#'
+geo_wkt <- function(x = character()) {
+  x <- vec_cast(x, character())
+  new_geo_wkt(x)
+}
+
+#' @rdname geo_wkt
+#' @export
+new_geo_wkt <- function(x = character()) {
+  vec_assert(x, character())
+  new_vctr(x, class = c("geo_wkt", "geo"))
+}
+
+#' @rdname geo_wkt
+#' @export
+is_geo_wkt <- function(x) {
+  inherits(x, "geo_wkt")
+}
+
+#' @rdname geo_wkt
+#' @export
+validate_geo_wkt <- function(x) {
+  abort("Not implemented")
+}
+
+#' @rdname geo_wkt
+#' @export
+vec_ptype_abbr.vctrs_percent <- function(x, ...) {
+  "wkt"
+}
---
+++ tests/testthat/test-geo-wkt.R
@@ -0,0 +1,8 @@
+
+test_that("geo_wkt class works", {
+  wkt <- geo_wkt("POINT (30 10)")
+  expect_output(print(wkt), "geo_wkt")
+  expect_is(wkt, "geo_wkt")
+  expect_true(is_geo_wkt(wkt))
+  expect_true(vec_is(wkt))
+})
