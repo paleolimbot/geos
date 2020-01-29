6703fca4466a0ba51e53d271c68056438e4e6a19
Dewey Dunnington <dewey@fishandwhistle.net>
2020-01-29 15:34:21 -0800

    fix makevars, get geosversion implemented

--- src/geos-version.cpp
+++ src/geos-version.cpp
@@ -3,9 +3,8 @@
 #define GEOS_USE_ONLY_R_API
 #include <geos_c.h>
 #include <Rcpp.h>
-using namespace Rcpp;
 
 // [[Rcpp::export]]
 std::string geos_version_impl() {
-    return GEOSversion();
+  return GEOSversion();
 }
