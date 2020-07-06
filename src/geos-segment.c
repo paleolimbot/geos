a2a6e2199bd78fb0a014155ecce95cd027f75b87
Dewey Dunnington <dewey@fishandwhistle.net>
2020-07-06 16:21:31 -0300

    re-use ptrs

--- src/geos-segment.c
+++ src/geos-segment.c
@@ -2,40 +2,45 @@
 #include "libgeos.h"
 #include "Rinternals.h"
 
-SEXP geos_c_segment_intersection(SEXP ax0, SEXP ay0, SEXP ax1, SEXP ay1,
-                                 SEXP bx0, SEXP by0, SEXP bx1, SEXP by1) {
-  R_xlen_t size = Rf_xlength(ax0);
+SEXP geos_c_segment_intersection(SEXP Sax0, SEXP Say0, SEXP Sax1, SEXP Say1,
+                                 SEXP Sbx0, SEXP Sby0, SEXP Sbx1, SEXP Sby1) {
+  R_xlen_t size = Rf_xlength(Sax0);
   SEXP resultX = PROTECT(Rf_allocVector(REALSXP, size));
   SEXP resultY = PROTECT(Rf_allocVector(REALSXP, size));
 
   GEOSContextHandle_t handle = GEOS_init_r();
-  double ax0i, ay0i, ax1i, ay1i, bx0i, by0i, bx1i, by1i, cxi, cyi;
   int resultCode;
 
-  for (R_xlen_t i = 0; i < size; i++) {
-    ax0i = REAL(ax0)[i]; ay0i = REAL(ay0)[i];
-    ax1i = REAL(ax1)[i]; ay1i = REAL(ay1)[i];
-    bx0i = REAL(bx0)[i]; by0i = REAL(by0)[i];
-    bx1i = REAL(bx1)[i], by1i = REAL(by1)[i];
+  double* ax0 = REAL(Sax0);
+  double* ay0 = REAL(Say0);
+  double* ax1 = REAL(Sax1);
+  double* ay1 = REAL(Say1);
+  double* bx0 = REAL(Sbx0);
+  double* by0 = REAL(Sby0);
+  double* bx1 = REAL(Sbx1);
+  double* by1 = REAL(Sby1);
+  double* cx = REAL(resultX);
+  double* cy = REAL(resultY);
 
-    if (R_FINITE(ax0i) && R_FINITE(ay0i) &&
-        R_FINITE(ax1i) && R_FINITE(ay1i) &&
-        R_FINITE(bx0i) && R_FINITE(by0i) &&
-        R_FINITE(bx1i) && R_FINITE(by1i)) {
+  for (R_xlen_t i = 0; i < size; i++) {
+    if (R_FINITE(ax0[i]) && R_FINITE(ay0[i]) &&
+        R_FINITE(ax1[i]) && R_FINITE(ay1[i]) &&
+        R_FINITE(bx0[i]) && R_FINITE(by0[i]) &&
+        R_FINITE(bx1[i]) && R_FINITE(by1[i])) {
 
       resultCode = GEOSSegmentIntersection_r(
         handle,
-        ax0i, ay0i,
-        ax1i, ay1i,
-        bx0i, by0i,
-        bx1i, by1i,
-        &cxi, &cyi
+        ax0[i], ay0[i],
+        ax1[i], ay1[i],
+        bx0[i], by0[i],
+        bx1[i], by1[i],
+        &cx[i], &cy[i]
       );
 
       // returns 0 on error, -1 if segments do not intersect
       if (resultCode == 1) {
-        REAL(resultX)[i] = cxi;
-        REAL(resultY)[i] = cyi;
+        REAL(resultX)[i] = cx[i];
+        REAL(resultY)[i] = cy[i];
       } else if (resultCode == -1) {
         REAL(resultX)[i] = NAN;
         REAL(resultY)[i] = NAN;
@@ -61,29 +66,27 @@ SEXP geos_c_segment_intersection(SEXP ax0, SEXP ay0, SEXP ax1, SEXP ay1,
   return result;
 }
 
-SEXP geos_c_orientation_index(SEXP Ax, SEXP Ay, SEXP Bx, SEXP By, SEXP Px, SEXP Py) {
-  R_xlen_t size = Rf_xlength(Ax);
+SEXP geos_c_orientation_index(SEXP SAx, SEXP SAy, SEXP SBx, SEXP SBy, SEXP SPx, SEXP SPy) {
+  R_xlen_t size = Rf_xlength(SAx);
   SEXP result = PROTECT(Rf_allocVector(INTSXP, size));
 
   GEOSContextHandle_t handle = GEOS_init_r();
 
-  double Axi, Ayi, Bxi, Byi, Pxi, Pyi;
+  double* Ax = REAL(SAx);
+  double* Ay = REAL(SAy);
+  double* Bx = REAL(SBx);
+  double* By = REAL(SBy);
+  double* Px = REAL(SPx);
+  double* Py = REAL(SPy);
   int resultCode;
 
   for (R_xlen_t i = 0; i < size; i++) {
-    Axi = REAL(Ax)[i];
-    Ayi = REAL(Ay)[i];
-    Bxi = REAL(Bx)[i];
-    Byi = REAL(By)[i];
-    Pxi = REAL(Px)[i];
-    Pyi = REAL(Py)[i];
-
-    if (R_FINITE(Axi) && R_FINITE(Ayi) && R_FINITE(Bxi) && R_FINITE(Byi) &&
-        R_FINITE(Pxi) && R_FINITE(Pyi)) {
+    if (R_FINITE(Ax[i]) && R_FINITE(Ay[i]) && R_FINITE(Bx[i]) && R_FINITE(By[i]) &&
+        R_FINITE(Px[i]) && R_FINITE(Py[i])) {
       resultCode = GEOSOrientationIndex_r(
         handle,
-        Axi, Ayi, Bxi, Byi,
-        Pxi, Pyi
+        Ax[i], Ay[i], Bx[i], By[i],
+        Px[i], Py[i]
       );
 
       // returns 2 on error
