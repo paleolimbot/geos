599bcf4f3d0daab98c3cef66058772bb26bcf4da
Dewey Dunnington <dewey@fishandwhistle.net>
2021-03-26 21:54:36 -0300

    CRS support for sf

--- R/compat-sf.R
+++ R/compat-sf.R
@@ -2,20 +2,27 @@
 #' @rdname as_geos_geometry
 #' @export
 as_geos_geometry.sfc <- function(x, ...) {
-  geos_read_wkb(sf::st_as_binary(x, EWKB = FALSE))
+  geos_read_wkb(sf::st_as_binary(x, EWKB = FALSE), crs = sf::st_crs(x))
 }
 
 #' @rdname as_geos_geometry
 #' @export
 as_geos_geometry.sf <- function(x, ...) {
-  geos_read_wkb(sf::st_as_binary(sf::st_geometry(x), EWKB = FALSE))
+  geos_read_wkb(sf::st_as_binary(sf::st_geometry(x), EWKB = FALSE),  crs = sf::st_crs(x))
 }
 
 # dynamically exported
 st_as_sfc.geos_geometry <- function(x, ...) {
-  # use wk's exporter to handle the empty point
-  wk_wkb <- as_wkb.geos_geometry(x)
-  sf::st_as_sfc(structure(wk_wkb, class = "WKB"))
+  if (geos_version() >= "3.9.1") {
+    wk_wkb <- geos_write_wkb(x)
+  } else {
+    # use wk's exporter to handle the empty point for older GEOS
+    wk_wkb <- as_wkb.geos_geometry(x) # nocov
+  }
+
+  sfc <- sf::st_as_sfc(structure(wk_wkb, class = "WKB"))
+  sf::st_crs(sfc) <- sf::st_crs(attr(x, "crs", exact = TRUE))
+  sfc
 }
 
 st_as_sf.geos_geometry <- function(x, ...) {
--- tests/testthat/test-compat-sf.R
+++ tests/testthat/test-compat-sf.R
@@ -25,3 +25,23 @@ test_that("conversion to sf works", {
   expect_equal(sf::st_as_sf(geom), sf)
   expect_equal(sf::st_as_sfc(geom), sfc)
 })
+
+test_that("conversion from sf propagates CRS", {
+  skip_if_not_installed("sf")
+
+  sfc <- sf::st_sfc(sf::st_point(c(0, 1)), crs = 4326)
+  sf <- sf::st_as_sf(new_data_frame(list(geometry = sfc)))
+
+  expect_identical(wk_crs(as_geos_geometry(sfc)), sf::st_crs(4326))
+  expect_identical(wk_crs(as_geos_geometry(sf)), sf::st_crs(4326))
+})
+
+test_that("conversion to sf propagates CRS", {
+  skip_if_not_installed("sf")
+
+  sfc <- sf::st_sfc(sf::st_point(c(0, 1)), crs = 4326)
+  sf <- sf::st_as_sf(new_data_frame(list(geometry = sfc)))
+
+  expect_identical(sf::st_as_sfc(geos_read_wkt("POINT (0 1)", crs = 4326)), sfc)
+  expect_identical(sf::st_as_sf(geos_read_wkt("POINT (0 1)", crs = 4326)), sf)
+})
