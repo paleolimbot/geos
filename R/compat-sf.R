fe1fd005292895e59af2ac276b37e5f5d987d019
Dewey Dunnington <dewey@fishandwhistle.net>
2022-08-17 09:41:50 -0300

    lean in to the default method based on the writer

--- R/compat-sf.R
+++ R/compat-sf.R
@@ -1,16 +1,4 @@
 
-#' @rdname as_geos_geometry
-#' @export
-as_geos_geometry.sfc <- function(x, ...) {
-  geos_read_wkb(sf::st_as_binary(x), crs = sf::st_crs(x))
-}
-
-#' @rdname as_geos_geometry
-#' @export
-as_geos_geometry.sf <- function(x, ...) {
-  geos_read_wkb(sf::st_as_binary(sf::st_geometry(x)), crs = sf::st_crs(x))
-}
-
 # dynamically exported
 st_as_sfc.geos_geometry <- function(x, ...) {
   sfc <- wk::wk_handle(x, wk::sfc_writer())
